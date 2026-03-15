ffb3de6858b1c876f2f7802838cc2ae9049c0137
Horsepower Gmail Account <46545547+Horsepower4991@users.noreply.github.com>
2026-03-15 15:43:19 -0400

    Trying to get Theta Error Down During Autos

--- 2026Rebuilt/src/main/java/frc/robot/RobotContainer.java
+++ 2026Rebuilt/src/main/java/frc/robot/RobotContainer.java
@@ -34,7 +34,7 @@ public class RobotContainer {
   private final SendableChooser<Command> autoChooser;
 
   private double MaxSpeed = TunerConstants.kSpeedAtTestVolts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-  private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+  private double MaxAngularRate = RotationsPerSecond.of(1).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
   /* Setting up bindings for necessary control of the swerve drive platform */
   private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
