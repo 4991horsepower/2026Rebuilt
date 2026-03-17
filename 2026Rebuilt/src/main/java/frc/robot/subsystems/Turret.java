d4c60f8cde69594ac1fcc60667e5d5bb7bf6ae7b
Horsepower Gmail Account <46545547+Horsepower4991@users.noreply.github.com>
2026-03-17 18:29:41 -0400

    missing parathesis

--- 2026Rebuilt/src/main/java/frc/robot/subsystems/Turret.java
+++ 2026Rebuilt/src/main/java/frc/robot/subsystems/Turret.java
@@ -149,7 +149,7 @@ public class Turret extends SubsystemBase {
             SmartDashboard.putNumber("Actual Shooter Speed", m_Wheel.getVelocity().getValueAsDouble());
         }
 
-        m_Turret.setPosition((m_Turret.getPosition().getValueAsDouble() % 1) * (Math.abs(m_Turret.getPosition().getValueAsDouble()/m_Turret.getPosition().getValueAsDouble()));
+        m_Turret.setPosition((m_Turret.getPosition().getValueAsDouble() % 1) * (Math.abs(m_Turret.getPosition().getValueAsDouble()/m_Turret.getPosition().getValueAsDouble())));
 
     setHoodAngle(setHoodAngle);
     }
