0103baf08d242869bc28a628c76b2e7b4c22f0b6
Horsepower Gmail Account <46545547+Horsepower4991@users.noreply.github.com>
2026-03-17 20:09:17 -0400

    Fix compilation issues

--- 2026Rebuilt/src/main/java/frc/robot/RobotContainer.java
+++ 2026Rebuilt/src/main/java/frc/robot/RobotContainer.java
@@ -37,7 +37,7 @@ public class RobotContainer {
   private final SendableChooser<Command> autoChooser;
 
   private double MaxSpeed = TunerConstants.kSpeedAtTestVolts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
-  private double MaxAngularRate = RotationsPerSecond.of(3/4).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+  private double MaxAngularRate = RotationsPerSecond.of(1.0).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
 
   /* Setting up bindings for necessary control of the swerve drive platform */
   private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
